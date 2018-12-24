86475a5887334b484c628c78bdab55704f36cde2
henrisaing <henri.saing@gmail.com>
2018-12-24 14:40:15 -0500

    Removes placeholder default text.

--- shotclock/resources/views/home.blade.php
+++ shotclock/resources/views/home.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <div class="panel panel-default">
-        <div class="panel-heading">?????</div>
+        <div class="panel-heading">Active</div>
 
         <div class="panel-body">
         
@@ -21,29 +21,16 @@
           </thead>
 
           <tbody>
-            <tr>
-              <td>bob</td>
-              <td>4.3</td>
-              <td>4.3</td>
-              <td>0.03</td>
-              <td><?php echo(new DateTime())->format('H:i:s') ?></td>
-              <td><button><i class="fas fa-plus"></i></button></td>
-            </tr>
           </tbody>
         </table>
+        
         <input type="text" name="new-name" id="person-name">
         <button class="new-person" id="male">add <i class="fas fa-mars"></i></button>
         <button class="new-person" id="female">add <i class="fas fa-venus"></i></button>
-          <!-- @if (session('status'))
-            <div class="alert alert-success">
-              {{ session('status') }}
-            </div>
-          @endif
-
-          You are logged in! 
-          {{ print_r(session('status')) }} -->
         </div>
       </div>
+
+
     </div>
   </div>
 </div>
